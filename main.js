e9cf1a694f1c465c1dd9cd099e3c9af22f79d641
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-02-23 20:55:44 +0000

    skeleton html and pseudo code (js) added

--- main.js
+++ main.js
@@ -3,3 +3,14 @@
 // 2 pokemon vs each other - I choose you! {name} --- fight cat (type)
 // add score for ea win
 // load new pokemon after each round
+
+// while loop for game?
+
+// define HTML variables
+// use api to get 2x random pokemon
+// store pokemon types for 1 round
+// buttons to choose your pokemon
+// compare pokemon cats againts ea other
+// if win add 1
+// if lose game over
+// new game?
