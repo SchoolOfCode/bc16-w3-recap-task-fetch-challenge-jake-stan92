184fec28cb97780ac022473d925e1fc48d611e1c
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-02-23 20:50:03 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// 151 total pokemon
+// https://pokeapi.co/api/v2/pokemon/{randint will return random} // name will return specific
+// 2 pokemon vs each other - I choose you! {name} --- fight cat (type)
+// add score for ea win
+// load new pokemon after each round
